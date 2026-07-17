ae47581fea70c08bf0efdeab30ebc8947358e8d4
Joel Karie <joelkarie@gmail.com>
2026-07-17 11:55:27 -0400

    Initial Commit

---
+++ Atlas Paradiso/Atlas_ParadisoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Atlas_ParadisoApp.swift
+//  Atlas Paradiso
+//
+//  Created by Joel Karie on 7/17/26.
+//
+
+import SwiftUI
+
+@main
+struct Atlas_ParadisoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
