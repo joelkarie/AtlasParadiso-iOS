6f70d22dc0df112071faad6e3e9a07df52664f3b
Joel Karie <joelkarie@gmail.com>
2026-07-17 23:23:17 -0400

    Add APIClient

--- Atlas Paradiso/Networking/APIClient.swift
+++ Atlas Paradiso/Networking/APIClient.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+final class APIClient {
+    
+    static let shared = APIClient()
+    
+    private init() {}
+    
+    func fetchLocations() async throws -> [Location] {
+        let url = API.baseURL.appendingPathComponent("locations")
+        
+        let (data,_) = try await URLSession.shared.data(from: url) // The tuple is the body and the response (ie. 404, 500)
+        
+        let decoder = JSONDecoder()
+        
+        return try decoder.decode([Location].self, from: data)
+    }
+}
